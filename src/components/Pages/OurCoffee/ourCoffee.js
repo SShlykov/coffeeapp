744a6e0f56ab94461ebc203cf6910c733f163f1c
Sergey Shl <sshlykov@icovt.ru>
2019-02-12 23:09:32 +0700

    raw project v0.36 first searches/filters

--- src/components/Pages/OurCoffee/ourCoffee.js
+++ src/components/Pages/OurCoffee/ourCoffee.js
@@ -1,61 +1,77 @@
-import React from "react";
+import React, {Component} from "react";
 import girl from "./img/coffee_girl.jpg"
 import Item,{ BeansLogo} from "../../SmallComp"
 
-const OurCoffee = ({data}) => {
-    const items = data.map(item=>{
-        const {id, name, weight, country, price, imgSrc} = item;
-        return <Item key={id} id={id} name={name} weight={weight} country={country} price={price} imgSrc={imgSrc}/>
-    });
-    return(
-        <section className="shop">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-4 offset-2">
-                        <img className="shop__girl" src={girl} alt="girl"/>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="title">About our beans</div>
-                        <BeansLogo dark/>
-                        <div className="shop__text">
-                            Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
-                            <br/><br/>
-                            Afraid at highly months do things on at. Situation recommend objection do intention<br/>
-                            so questions. <br/>
-                            As greatly removed calling pleased improve an. Last ask him cold feel<br/>
-                            met spot shy want. Children me laughing we prospect answered followed. At it went<br/>
-                            is song that held help face.
+export default class OurCoffee extends Component {
+    state = {
+      term: ""
+    };
+    onType = e => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onSearchChange(term);
+    };
+    render() {
+        const items = this.props.data.map(item => {
+            const {id, name, weight, country, price, imgSrc} = item;
+            return <Item key={id} id={id} name={name} weight={weight} country={country} price={price} imgSrc={imgSrc}/>
+        });
+        return (
+            <section className="shop">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-4 offset-2">
+                            <img className="shop__girl" src={girl} alt="girl"/>
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="title">About our beans</div>
+                            <BeansLogo dark/>
+                            <div className="shop__text">
+                                Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+                                <br/><br/>
+                                Afraid at highly months do things on at. Situation recommend objection do intention<br/>
+                                so questions. <br/>
+                                As greatly removed calling pleased improve an. Last ask him cold feel<br/>
+                                met spot shy want. Children me laughing we prospect answered followed. At it went<br/>
+                                is song that held help face.
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="line"> </div>
-                <div className="row">
-                    <div className="col-lg-4 offset-lg-2">
-                        <form action="#" className="shop__search">
+                    <div className="line"> </div>
+                    <div className="row">
+                        <div className="col-lg-4 offset-lg-2">
                             <label className="shop__search-label" htmlFor="filter">Looking for</label>
-                            <input id="filter" type="text" placeholder="start typing here..." className="shop__search-input"/></form></div>
-                    <div className="col-lg-4">
-                        <div className="shop__filter">
-                            <div className="shop__filter-label">
-                                Or filter
+                            <input
+                                id="filter"
+                                type="text"
+                                placeholder="start typing here..."
+                                className="shop__search-input"
+                                value={this.state.term}
+                                onChange={this.onType}
+                            />
+                        </div>
+                        <div className="col-lg-4">
+                            <div className="shop__filter">
+                                <div className="shop__filter-label">
+                                    Or filter
+                                </div>
+                                <div className="shop__filter-group">
+                                    <button className="shop__filter-btn">Brazil</button>
+                                    <button className="shop__filter-btn">Kenya</button>
+                                    <button className="shop__filter-btn">Columbia</button>
+                                </div>
                             </div>
-                            <div className="shop__filter-group">
-                                <button className="shop__filter-btn">Brazil</button>
-                                <button className="shop__filter-btn">Kenya</button>
-                                <button className="shop__filter-btn">Columbia</button>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-10 offset-lg-1">
+                            <div className="shop__wrapper">
+                                {items}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-lg-10 offset-lg-1">
-                        <div className="shop__wrapper">
-                            {items}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 };
-export default OurCoffee;
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -19,12 +19,45 @@ export default class App extends Component {
             {id: 15, name:"AROMISTICO Coffee", weight:"1", country:"Brazil", price:"6.99"},
             {id: 21, name:"Solimo Coffee Beans", weight:"2", country:"Brazil", price:"10.73"},
             {id: 22, name:"Presto Coffee Beans", weight:"1", country:"Brazil", price:"15.99"},
-            {id: 23, name:"Solimo Coffee Beans", weight:"1", country:"Brazil", price:"6.99", imgSrc:"https://hhp-blog.s3.amazonaws.com/2018/07/iStock-673468996.jpg"},
+            {id: 23, name:"Solimo Coffee Beans", weight:"1", country:"Columbia", price:"6.99", imgSrc:"https://hhp-blog.s3.amazonaws.com/2018/07/iStock-673468996.jpg"},
             {id: 24, name:"Solimo Coffee Beans", weight:"2", country:"Brazil", price:"10.73"},
             {id: 25, name:"Solimo Coffee Beans", weight:"2", country:"Brazil", price:"10.73", imgSrc:"https://i0.wp.com/www.healthline.com/hlcmsresource/images/AN_images/AN275-cup-of-coffee-732x549-Thumb.jpg?w=756"},
             {id: 26, name:"Solimo Coffee Beans", weight:"2", country:"Brazil", price:"10.73"},
-        ]
+        ],
+        term: "",
+        filter: ""
+    };
+    onType = e => {
+        const term = e.target.value;
+        this.setState({term});
+        this.onSearchChange(term, e);
+    };
+    search = (arr, term) => {
+        if(term.length === 0){
+            return arr;
+        }
+        return arr.filter(item => {
+            return item.name.toLowerCase().indexOf(term.toLowerCase())>-1;
+        });
+    };
+    filter(items, filter) {
+        switch (filter) {
+            case "Brazil":
+                return items.filter(item => item.country === "Brazil");
+            case "Kenya":
+                return items.filter(item => item.country === "Kenya");
+            case "Columbia":
+                return items.filter(item => item.country === "Columbia");
+            default:
+                return items;
+        }
     }
+    onSearchChange = (term) => {
+        this.setState({ term });
+    };
+    onFilterChange = filter => {
+        this.setState({ filter });
+    };
   render() {
       let bestId = [1,2,3];
       let FYPId = [10,11,12,13,14,15];
@@ -35,28 +68,34 @@ export default class App extends Component {
           }
           return null
       })
-      const bestItems = searchItems(this.state.data, bestId);
-      const FYPItems = searchItems(this.state.data, FYPId);
-      const regularItems = searchItems(this.state.data, regularId);
+      const {data, term, filter} = this.state;
+      const visibleData = this.filter(this.search(data, term), filter);
+      const bestItems = searchItems(visibleData, bestId);
+      const FYPItems = searchItems(visibleData, FYPId);
+      const regularItems = searchItems(visibleData, regularId);
     return (
       <Router>
           <>
+              <input onChange={this.onType} value={this.state.term}/>
               <Switch>
                   <Route exact path="/" component={FirstScreen}/>
                   <Route path="/ourcoffee/" component={() =>  <FirstScreen heading="Our coffee"/>}/>
                   <Route path="/foryourpleasure/" component={() => <FirstScreen heading="For your pleasure"/>}/>
                   <Route path="/contacts/" component={() => <FirstScreen heading="Contact Us"/>}/>
               </Switch>
-              <Switch>
                   <Route exact path="/" component={() => <MainPage data={bestItems}/>}/>
-                  <Route exact path="/ourcoffee/" component={() => <OurCoffee data={regularItems}/>}/>
+                  <Route exact path="/ourcoffee/" component={() => <OurCoffee
+                                                                        data={regularItems}
+                                                                        filter={filter}
+                                                                        onFilterChange={this.onFilterChange}
+                                                                        onSearchChange={this.onSearchChange}
+                                                                    />}/>
                   <Route exact path="/ourcoffee/:id" component={({match}) => {
                       const {id} = match.params;
                       return <ProductPage id={id} />
                   }}/>
                   <Route exact path="/foryourpleasure/" component={() => <ForYourPleasure data={FYPItems}/>}/>
                   <Route exact path="/contacts/" component={Contacts}/>
-              </Switch>
               <Footer/>
           </>
       </Router>
